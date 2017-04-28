e5de2c95843d1405d01949c8c6f955070d9ca630
Miles Helm <mileshelm@Miless-MacBook-Pro.local>
2017-04-27 21:45:35 -0400

    keys

--- keys.js
+++ keys.js
@@ -1,9 +1,9 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '8XjZlK04b1KAa6egH8G7Ag9UT',
+  consumer_secret: 'WzO97NDMHQWzmP7ueWUDIv8DWGucQMVWM1txY1167LAu9tuXDd',
+  access_token_key: '855166905698668544-AVKkAF5pwj6JbfzhJ7yVO1AabWN93si',
+  access_token_secret: 'IXNSzJ9RjNKVApmTctxWCEupU25RC5mEC8kJ1QLy5Ts4q',
 }
 
